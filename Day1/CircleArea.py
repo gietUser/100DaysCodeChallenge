f99797a43e4c37c693b9c96c9e3e247303a4d456
= <ashif8984@gmail.com>
2019-08-01 00:31:14 +0530

    commiting 2 codes

---
+++ Day1/CircleArea.py
@@ -0,0 +1,9 @@
+# area of circle
+
+import math
+
+radius = float(input("Enter the radius:"))
+area = math.pi * radius * radius
+
+print("Area of the Circle: {0:.2f}".format(area))
+
---
+++ Day1/Factorial.py
@@ -0,0 +1,14 @@
+# Factorial of a number
+
+def factorial(num):
+	if num == 1:
+		return 1
+	else:
+		return num * factorial(num-1)
+
+
+print("Enter a number")
+num = int(input())
+res = factorial(num)
+
+print("Factorial of {0} = {1}".format(num, res))
