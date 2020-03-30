2c2af92b50b0df99c82e12d832f55ff0daabf972
= <ashif8984@gmail.com>
2020-03-30 11:26:20 +0530

    Added formula for permiter

--- Progress/Day1/CircleArea.py
+++ Progress/Day1/CircleArea.py
@@ -4,6 +4,8 @@ import math
 
 radius = float(input("Enter the radius:"))
 area = math.pi * radius * radius
+permi = math.pi * 2 * radius
 
 print("Area of the Circle: {0:.2f}".format(area))
+print("Perimeter of the Circle: {0:.2f}".format(permi))
 
