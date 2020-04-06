302bec20c31c8e64568e18bdeb8d2e76fea0b2ba
= <ashif8984@gmail.com>
2020-04-06 22:30:40 +0530

    Day 7: GuessNumber.py Added

---
+++ Progress/Day8/NumberGuess.py
@@ -0,0 +1,29 @@
+# Guess Number Game
+
+import random  
+
+# Generate number
+num = random.randint(0, 9)
+print(num)
+
+count = 0
+guess = 0
+
+while guess!=num and guess!="exit":
+
+    guess = input("Guess a number")
+    if guess == 'exit':
+        break
+
+    guess = int(guess)
+    count += 1
+
+    if guess < num:
+        print("Guess a higher number")
+    
+    elif guess > num:
+        print("Guess a lower number")
+    
+    else:
+        print("You guess it Right!!")
+        print("The Number was {0} and you took {1} attempts to guess".format(num, count))
